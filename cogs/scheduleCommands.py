af806bfc603382ef411b7a1992ede737f64b27d9
yamaan93 <yamaan93@gmail.com>
2021-01-04 20:13:06 -0500

    google sheets integration has begun
    but i really am too tired for this rn

--- cogs/scheduleCommands.py
+++ cogs/scheduleCommands.py
@@ -4,6 +4,48 @@ import pandas as pd
 from tabulate import tabulate
 import functools
 
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow,Flow
+from google.auth.transport.requests import Request
+import os
+import pickle
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# here enter the id of your google sheet
+SAMPLE_SPREADSHEET_ID_input = '1oQDgT7eO0zSD0EBQUZ7h7DzIM-84Slw91XDT7xFmzQc'
+SAMPLE_RANGE_NAME = 'A1:AA1000'
+
+def main():
+    global values_input, service
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES) # here enter the name of your downloaded JSON file
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values_input = result_input.get('values', [])
+
+    if not values_input: # and not values_expansion:
+        print('No data found.')
+
+main()
+
+df=pd.DataFrame(values_input[1:], columns=values_input[0])
 
 FLOAT_COLUMNS = ('Floats',)
 BOOLEAN_COLUMNS = ('Booleans',)
