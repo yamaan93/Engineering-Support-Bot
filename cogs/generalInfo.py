a6c68f9d10fa0918a15a65d4b48529ca446f655c
yamaan93 <yamaan93@gmail.com>
2021-01-10 19:27:06 -0500

    Update generalInfo.py

--- cogs/generalInfo.py
+++ cogs/generalInfo.py
@@ -13,7 +13,7 @@ class generalInfo(commands.Cog):
     
     @commands.command()
     async def textbooks(self,ctx):
-        await ctx.send('test')
+        await ctx.send('**Mandatory purchase list** \n It seems there is a *lot* of questions on what exactly you need to buy so I thought I would simplify it for you guys and put the links for all the mandatory purchases in their cheapest forms.  \n Purchase List: \n Physics code bundle:``` \n  - https://bookstore.uwo.ca/product/cebcodeid33819 \n ```Physics Lab Book:``` \n - https://bookstore.uwo.ca/product/88000099137 \n ```Business E-Book:``` \n  -https://bookstore.uwo.ca/product/cebebookid9687792 \n ```chemistry Pearson Mastering``` \n -https://bookstore.uwo.ca/product/cebcodeid30214 \n ```chemistry lab manual`` \n -https://www.vitalsource.com/en-ca/products/chemistry-1302a-b-discovering-chemical-energetics-department-of-chemistry-v9781533925688 \n ```Calculus Pearson Mastering``` \n -https://bookstore.uwo.ca/product/cebcodeid23905 \n ``Statics Mastering``` \n https://bookstore.uwo.ca/product/cebcodeid23237 ')
         
 def setup(client):
     client.add_cog(generalInfo(client))
