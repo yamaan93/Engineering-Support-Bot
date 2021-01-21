9ff75578f09782c7c90f01237f8762f67719b306
yamaan93 <yamaan93@gmail.com>
2021-01-21 18:13:34 -0500

    beef command

--- cogs/generalInfo.py
+++ cogs/generalInfo.py
@@ -14,6 +14,9 @@ class generalInfo(commands.Cog):
     @commands.command()
     async def textbooks(self,ctx):
         await ctx.send('**Mandatory purchase list** \n It seems there is a *lot* of questions on what exactly you need to buy so I thought I would simplify it for you guys and put the links for all the mandatory purchases in their cheapest forms.  \n Purchase List: \n Physics code bundle: \n  ```- https://bookstore.uwo.ca/product/cebcodeid33819 ``` \nPhysics Lab Book: \n```  - https://bookstore.uwo.ca/product/88000099137```\nBusiness E-Book:\n ```-https://bookstore.uwo.ca/product/cebebookid9687792 ``` \nchemistry Pearson Mastering \n```-https://bookstore.uwo.ca/product/cebcodeid30214``` \n chemistry lab manual \n ```-https://www.vitalsource.com/en-ca/products/chemistry-1302a-b-discovering-chemical-energetics-department-of-chemistry-v9781533925688``` \n Calculus Pearson Mastering \n ```-https://bookstore.uwo.ca/product/cebcodeid23905``` \n Statics Mastering \n ```https://bookstore.uwo.ca/product/cebcodeid23237``` ')
-        
+    @commands.command()
+    @commands.has_role('Leaders')   
+    async def beefCounter(self,ctx,perp1,perp2):
+        counter = pd.read_excel()
 def setup(client):
     client.add_cog(generalInfo(client))
